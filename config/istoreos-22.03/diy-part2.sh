736057500ad6aa385d0b1d467b84936683373dea
ophub <68696949+ophub@users.noreply.github.com>
2023-12-22 19:02:22 -0800

    Add istoreos

---
+++ config/istoreos-22.03/diy-part2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/istoreos/istoreos / Branch: istoreos-22.03
+#========================================================================================================================
+
+# ------------------------------- Main source started -------------------------------
+#
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+# sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='istoreos'" >>package/base-files/files/etc/openwrt_release
+
+# Modify default IP（FROM 192.168.100.1 CHANGE TO 192.168.1.1）
+sed -i 's/192.168.100.1/192.168.1.1/g' package/istoreos-files/Makefile
+#
+# ------------------------------- Main source ends -------------------------------
+
+# ------------------------------- Other started -------------------------------
+#
+# Add luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+#
+# Apply patch
+# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
+#
+# ------------------------------- Other ends -------------------------------
